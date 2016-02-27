35e23f5bf5a85c597da03a6f24dfe1ad67eb3ff5
Juan Carlos <juanjcsr@gmail.com>
2016-02-26 18:34:23 -0600

    initial commit

---
+++ routers/hello.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"api.jwt.auth/controllers"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func SetHelloRoutes(router *mux.Router) *mux.Router {
+	router.Handle("/test/hello",
+		negroni.New(
+			negroni.HandlerFunc(controllers.HelloController),
+		)).Methods("GET")
+
+	return router
+}
